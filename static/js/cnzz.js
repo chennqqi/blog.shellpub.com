5daaf35b16db57db5a2c47aac3d91995ac3a707e
CHENQ <chennqqi@qq.com>
2017-03-19 11:45:15 +0800

    add custom css and js

---
+++ static/js/cnzz.js
@@ -0,0 +1,3 @@
+document.write(unescape("%3Cspan style='display:none;' id='cnzz_stat_icon_1261528604'%3E%3C/span%3E%3Cscript src='" + "https://s11.cnzz.com/z_stat.php%3Fid%3D1261528604' type='text/javascript'%3E%3C/script%3E"));
+document.getElementById("cnzz_stat_icon_1261528604").style.display = "none";
+
